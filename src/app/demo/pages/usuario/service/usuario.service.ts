3857df635eb0b90a12cbd5f157802672117fbbdd
gmora-asd <lmora@grupoasd.com.co>
2025-03-31 22:38:27 -0500

    feat: ajustes para frontend crear usuario

--- src/app/demo/pages/usuario/service/usuario.service.ts
+++ src/app/demo/pages/usuario/service/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/models/usuario';
+import { UsuarioRs } from 'src/app/models/usuarioRs';
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 
@@ -20,5 +21,13 @@ export class UsuarioService {
 
   getUsuarios(): Observable<Usuario[]> {
     return this.backendService.get(environment.apiUrl, this.api, "listar");
+  }
+
+  guardarUsuario(usuario: Usuario): Observable<UsuarioRs> {
+    return this.backendService.post(environment.apiUrl, this.api, "guardar-usuario", usuario);
+  }
+
+  actualizarUsuario(usuario: Usuario): Observable<UsuarioRs> {
+    return this.backendService.post(environment.apiUrl, this.api, "actualizar-usuario", usuario);
   }
 }
