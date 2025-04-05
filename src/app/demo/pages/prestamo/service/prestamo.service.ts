02eab7c43664014220f862cb67bd0af99ae9ed12
880515_HaroldPinto <harolpintosuarez54321@gmail.com>
2025-04-05 17:09:26 -0500

    creacion de componentes , servicios.

---
+++ src/app/demo/pages/prestamo/service/prestamo.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { BackendService } from 'src/app/services/backend.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrestamoService {
+  urlApi = environment.apiUrl;
+
+  constructor(private backendService: BackendService) {
+    this.test();
+  }
+
+  test() {
+    this.backendService.get(this.urlApi, "app", "test").subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
+  }
+}
