c450f0fcdc0976f3100c94c37f6ff8a6362ff538
gmora-asd <lmora@grupoasd.com.co>
2025-04-12 09:52:24 -0500

    feat: ajustes en modulo de gestion de libros

--- src/app/demo/pages/autor/service/autor.service.ts
+++ src/app/demo/pages/autor/service/autor.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Autor} from 'src/app/models/autor';
-import { AutorRs } from 'src/app/models/autorRs';
-
 import { BackendService } from 'src/app/services/backend.service';
 import { environment } from 'src/environments/environment';
 
@@ -10,25 +6,22 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AutorService {
-  private api = `autor`;
+  urlApi = environment.apiUrl;
 
-  constructor(private backendService: BackendService) { 
-    this.testService();
+  constructor(private backendService: BackendService) {
+    this.test();
   }
 
-  testService() {
-    this.backendService.get(environment.apiUrl, this.api, "test");
-  }
-
-  getAutor(): Observable<Autor[]> {
-    return this.backendService.get(environment.apiUrl, this.api, "listar");
-  }
-
-  guardarAutor(Autor: Autor): Observable<AutorRs> {
-    return this.backendService.post(environment.apiUrl, this.api, "guardar-Autor", Autor);
-  }
-
-  actualizarAutor(Autor: Autor): Observable<AutorRs> {
-    return this.backendService.post(environment.apiUrl, this.api, "actualizar-Autor", Autor);
+  test() {
+    this.backendService.get(this.urlApi, "app", "test").subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      }
+    );
   }
 }
--- src/app/models/autor.ts
+++ src/app/models/autor.ts
@@ -1,6 +1,8 @@
+import { Nacionalidad } from "./nacionalidad";
+
 export class Autor {
-    idAutor?: number;       
-    nombre: string;          
-    nacionalidad: string;      
-    fechaNacimiento: Date;    
-  }
+    idAutor: number;
+    nombre: string;
+    nacionalidad?: Nacionalidad;
+    fechaNacimiento?: Date;
+}
