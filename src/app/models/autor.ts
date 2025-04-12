66be013719f5b2ccffdbf64a5f478e94bae76598
Julian Sierra <juliansierra69@gmail.com>
2025-04-12 09:04:29 -0500

    Cambios modulos

---
+++ src/app/models/autor.ts
@@ -0,0 +1,6 @@
+export class autor {
+    id_autor: number;
+    nombre: string;
+    nacionalidad?: string;
+    fecha_nacimiento?: Date;
+}
---
+++ src/app/models/libro.ts
@@ -0,0 +1,11 @@
+class Libro {
+    id_libro: number;
+    titulo: string;
+    id_autor: number;
+    anio_publicacion?: number;
+    categoria?: string;
+    existencias: number;
+    año_publicacion?: string;
+}
+
+export default Libro;
