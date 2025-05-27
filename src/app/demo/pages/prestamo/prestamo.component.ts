ae37beaa9a354677d9d29dcbb1fdd178a52d48a9
YeiLop <lopezlopezyeisonjose@gmail.com>
2025-05-26 20:40:58 -0500

    cargue masivo

--- src/app/demo/pages/prestamo/prestamo.component.ts
+++ src/app/demo/pages/prestamo/prestamo.component.ts
@@ -1,219 +1,197 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component } from '@angular/core';
-import { CommonModule, DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageUtils } from 'src/app/utils/message-utils';
+import { UsuarioService } from '../usuario/service/usuario.service';
+import { Usuario } from 'src/app/models/usuario';
+import { LibroService } from '../libro/service/libro.service';
+import { Libro } from 'src/app/models/libro';
 import { PrestamoService } from './service/prestamo.service';
 import { Prestamo } from 'src/app/models/prestamo';
-import Swal, { SweetAlertIcon } from 'sweetalert2';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-  AbstractControl,
-  FormsModule,
-  ReactiveFormsModule
-} from '@angular/forms';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
-import { Usuario } from 'src/app/models/usuario';
-import { Libro } from 'src/app/models/libro';
+import { DateTimeUtils } from 'src/app/utils/date-utils';
 
 declare const bootstrap: any;
 
 @Component({
   selector: 'app-prestamo',
-  standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxSpinnerModule],
   templateUrl: './prestamo.component.html',
-  styleUrls: ['./prestamo.component.scss'],
-  providers: [DatePipe]
+  styleUrl: './prestamo.component.scss'
 })
 export class PrestamoComponent {
-  modoEdicion = false;
-  prestamos: Prestamo[] = [];
+  titleModal: string = '';
+  modoFormulario: string = '';
+  modalInstance: any;
   usuarios: Usuario[] = [];
   libros: Libro[] = [];
-  modalInstance: any;
-  modoFormulario = '';
-  titleModal = '';
-  msjSpinner = 'Cargando';
+  fechaDevolucion: string = ''; // ejemplo: '2025-04-07'
+  prestamos: Prestamo[] = [];
   prestamoSelected: Prestamo;
-  form: FormGroup;
-  fechaActual = new Date()
+  msjSpinner: string = '';
+
+  form: FormGroup = new FormGroup({
+    usuarioId: new FormControl(''),
+    libroId: new FormControl(''),
+    fechaDevolucion: new FormControl(''),
+    fechaEntrega: new FormControl('')
+  });
 
   constructor(
-    private prestamoService: PrestamoService,
-    private formBuilder: FormBuilder,
-    private datePipe: DatePipe,
-    private spinner: NgxSpinnerService
+    private readonly formBuilder: FormBuilder,
+    private readonly messageUtils: MessageUtils,
+    private readonly usuarioService: UsuarioService,
+    private readonly libroService: LibroService,
+    private readonly prestamoService: PrestamoService,
+    private readonly spinner: NgxSpinnerService,
+    private readonly dateTimeUtils: DateTimeUtils
   ) {
-    this.cargarUsuarios();
-    this.cargarLibros();
-    this.cargarListaPrestamos();
     this.cargarFormulario();
+    this.cargarListaUsuarios();
+    this.cargarListaLibros();
+    this.cargarPrestamos();
   }
-// En el método cargarFormulario()
-cargarFormulario() {
-  this.form = this.formBuilder.group({
-    idUsuario: ['', Validators.required],
-    idLibro: ['', Validators.required],
-    fechaDevolucion: ['', Validators.required],
-    fechaEntrega: ['']
-  });
-}
+
+  cargarPrestamos() {
+    this.prestamoService.listarPrestamos().subscribe({
+      next: (data) => {
+        this.prestamos = data;
+      },
+      error: (error) => {
+        console.log(error);
+        this.messageUtils.showMessage('Error', error.error.message, 'error');
+      }
+    });
+  }
+
+  cargarListaUsuarios() {
+    this.usuarioService.getUsuarios().subscribe({
+      next: (data) => {
+        this.usuarios = data;
+      },
+      error: (error) => {
+        this.messageUtils.showMessage('Error', error.error.message, 'error');
+      }
+    });
+  }
+
+  cargarListaLibros() {
+    this.libroService.getLibrosDisponiblesForPrestamo().subscribe({
+      next: (data) => {
+        this.libros = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  cargarFormulario() {
+    this.form = this.formBuilder.group({
+      usuarioId: ['', [Validators.required]],
+      libroId: ['', [Validators.required]],
+      fechaDevolucion: ['', [Validators.required]],
+      fechaEntrega: ['', [Validators.required]]
+    });
+  }
 
   get f(): { [key: string]: AbstractControl } {
     return this.form.controls;
   }
 
-  cargarListaPrestamos() {
-    this.spinner.show();
-    this.prestamoService.getPrestamos().subscribe({
-      next: (data) => {
-        this.prestamos = data;
-        this.prestamos.forEach((prestamo) => {
-          const usuario = this.usuarios.find(u => u.idUsuario === prestamo.idUsuario);
-          const libro = this.libros.find(l => l.idLibro === prestamo.idLibro);
-          prestamo['usuarioNombre'] = usuario?.nombre || 'N/A';
-          prestamo['libroTitulo'] = libro?.titulo || 'N/A';
-        });
-        this.spinner.hide();
-      },
-      error: (error) => {
-        this.showMessage('Error', error.error.message, 'error');
-        this.spinner.hide();
-      }
-    });
-  }
-
-  cargarUsuarios() {
-    this.prestamoService.getUsuarios().subscribe({
-      next: (data) => this.usuarios = data,
-      error: (error) => this.showMessage('Error', error.error.message, 'error')
-    });
-  }
-
-  cargarLibros() {
-    this.prestamoService.getLibros().subscribe({
-      next: (data) => this.libros = data,
-      error: (error) => this.showMessage('Error', error.error.message, 'error')
-    });
-  }
-
-  crearPrestamoModal(modoForm: string) {
-    this.modoFormulario = modoForm;
-    this.titleModal = modoForm === 'C' ? 'Crear Préstamo' : 'Editar Préstamo';
-    this.form.reset();
-
-    if (modoForm === 'E') {
-      this.form.addControl('estado', new FormControl('', Validators.required));
-      this.form.get('idUsuario')?.disable();
-      this.form.get('idLibro')?.disable();
-
-      this.form.get('fechaDevolucion')?.disable();
-    } else {
-      if (this.form.contains('estado')) {
-        this.form.removeControl('estado');
-      }
-    }
-
-    const modalElement = document.getElementById('crearPrestamoModal');
-    if (modalElement) {
-      modalElement.blur();
-      modalElement.setAttribute('aria-hidden', 'false');
-      if (!this.modalInstance) {
-        this.modalInstance = new bootstrap.Modal(modalElement);
-      }
-      this.modalInstance.show();
-    }
-  }
-
   cerrarModal() {
     this.form.reset();
-    if (this.form.contains('estado')) {
-      this.form.removeControl('estado');
-    }
+    this.form.markAsPristine();
+    this.form.markAsUntouched();
+    this.form.reset({
+      usuarioId: '',
+      libroId: '',
+      fechaDevolucion: ''
+    });
     if (this.modalInstance) {
       this.modalInstance.hide();
-      this.modalInstance = null;
     }
     this.prestamoSelected = null;
   }
 
   abrirModoEdicion(prestamo: Prestamo) {
-    this.crearPrestamoModal('E');
     this.prestamoSelected = prestamo;
-    this.modoEdicion = true;
-
     this.form.patchValue({
-      idUsuario: prestamo.idUsuario,
-      idLibro: prestamo.idLibro,
-      fechaPrestamo: this.datePipe.transform(prestamo.fechaPrestamo, 'yyyy-MM-dd'),
-      fechaDevolucion: this.datePipe.transform(prestamo.fechaDevolucion, 'yyyy-MM-dd'),
-      estado: prestamo.estado,
-      fechaEntrega: this.datePipe.transform(prestamo.fechaEntrega, 'yyyy-MM-dd')
+      usuarioId: prestamo.usuario.idUsuario,
+      libroId: prestamo.libro.idLibro,
+      fechaDevolucion: prestamo.fechaDevolucion
     });
+    this.crearModal('E');
   }
 
-  guardarActualizarPrestamo() {
-    if (this.form.valid) {
-      const rawValue = this.form.getRawValue();
-      const fechaHoy = new Date().toISOString().split('T')[0];
-
-      if (rawValue.fechaEntrega && rawValue.fechaEntrega < rawValue.fechaPrestamo) {
-        this.showMessage('Advertencia', 'La fecha de entrega no puede ser anterior a la fecha de préstamo.', 'warning');
-        return;
-      }
-
-      if (this.modoFormulario.includes('C')) {
-        const nuevoPrestamo = {
-          idUsuario: rawValue.idUsuario,
-          idLibro: rawValue.idLibro,
-          fechaPrestamo: new Date().toISOString().split('T')[0], // Fecha actual
-          fechaDevolucion: rawValue.fechaDevolucion
-        };
-        
-        this.prestamoService.guardarPrestamo(nuevoPrestamo).subscribe({
-          next: (data) => {
-            this.showMessage('Éxito', data.message, 'success');
-            this.cargarListaPrestamos();
-            this.cerrarModal();
-          },
-          error: (error) => this.showMessage('Error', error.error.message, 'error')
-        });
-      } else {
-        const updatedPrestamo = {
-          ...this.prestamoSelected,
-          estado: rawValue.estado,
-          fechaEntrega: rawValue.fechaEntrega
-        };
-        this.prestamoService.actualizarPrestamo(updatedPrestamo).subscribe({
-          next: (data) => {
-            this.showMessage('Éxito', data.message, 'success');
-            this.cargarListaPrestamos();
-            this.cerrarModal();
-          },
-          error: (error) => this.showMessage('Error', error.error.message, 'error')
-        });
-      }
+  crearModal(modoForm: string) {
+    this.cargarListaLibros();
+    this.titleModal = modoForm == 'C' ? 'Registrar Préstamo' : 'Editar Préstamo';
+    this.modoFormulario = modoForm;
+    const modalElement = document.getElementById('crearPrestamoModal');
+    if (modalElement) {
+      // Verificar si ya existe una instancia del modal
+      this.modalInstance ??= new bootstrap.Modal(modalElement);
+      this.modalInstance.show();
     }
   }
 
-  public showMessage(title: string, text: string, icon: SweetAlertIcon) {
-    Swal.fire({
-      title,
-      text,
-      icon,
-      confirmButtonText: 'Aceptar',
-      customClass: {
-        container: 'position-fixed',
-        popup: 'swal-overlay'
-      },
-      didOpen: () => {
-        const swalPopup = document.querySelector('.swal2-popup');
-        if (swalPopup) {
-          (swalPopup as HTMLElement).style.zIndex = '1060';
+  guardarPrestamo() {
+    this.msjSpinner = 'Guardando';   
+    if (this.modoFormulario.includes('C')) {
+      this.form.get('fechaEntrega')?.setValue(1);
+    } 
+    if (!this.form.valid) {
+      this.messageUtils.showMessage('Error', 'Por favor, complete todos los campos obligatorios.', 'error');
+      return;
+    }
+
+    if (this.form.valid) {
+      this.spinner.show();
+      this.prestamoService.guardarPrestamo(this.form.getRawValue()).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.spinner.hide();
+          this.messageUtils.showMessage('Éxito', data.message, 'success');
+          this.cargarPrestamos();
+          this.cerrarModal();
+        },
+        error: (error) => {
+          this.spinner.hide();
+          this.messageUtils.showMessage('Error', error.error.message, 'error');
         }
+      });
+    }
+  }
+
+  hacerEntregaLibro() {
+    this.msjSpinner = 'Entregando Libro';
+    this.spinner.show();
+    if (this.modoFormulario.includes('E')) {
+      this.form.get('usuarioId')?.setValue(this.prestamoSelected.usuario.idUsuario);
+      this.form.get('libroId')?.setValue(this.prestamoSelected.libro.idLibro);
+      this.form.get('fechaDevolucion')?.setValue(this.prestamoSelected.fechaDevolucion);
+    }
+    if (this.form.valid) {
+      const fechaForm = this.form.get('fechaEntrega')?.value;
+      if (fechaForm) {
+        const fecha = new Date(fechaForm);
+        this.prestamoSelected.fechaEntrega = this.dateTimeUtils.formatDateCurrentZone(fecha);
       }
-    });
+
+      this.prestamoService.entregarLibro(this.prestamoSelected).subscribe({
+        next: (data) => {
+          this.spinner.hide();
+          this.cerrarModal();
+          this.cargarPrestamos();
+          this.messageUtils.showMessage('Exito', data.message, 'success');
+        },
+        error: (error) => {
+          this.spinner.hide();
+          this.messageUtils.showMessage('Error', error.error.message, 'error');
+        }
+      });
+    }
   }
 }
